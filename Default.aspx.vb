1c26432793470e8d8d125d69fa09ce849659db0e
Dean Salter <dean.m.salter.com>
2021-06-05 19:54:14 +0100

    Add project files.

---
+++ Default.aspx.vb
@@ -0,0 +1,12 @@
+﻿Public Class _Default
+    Inherits Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+        System.Diagnostics.Debug.WriteLine("test2")
+        HelloWorldLabel.Text = "This label text was changed at runtime"
+    End Sub
+
+    Protected Sub button_click(sender As Object, e As EventArgs) Handles GreetButton.Click
+        HelloWorldLabel.Text = "This now has whatever you put in the input: , " + TextInput.Text
+    End Sub
+End Class
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="VBWebForm.Global_asax" Language="vb" %>
